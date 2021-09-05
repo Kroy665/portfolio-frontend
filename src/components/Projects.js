c12e850986311d9460dde7cf4c1db47c6d9dd291
Kroy665 <kroy665@gmail.com>
2021-09-05 21:44:37 +0530

    project edited

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Card, Button, Container, Row, Col } from 'react-bootstrap';
 function ProjectCard(props){
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
+    // https://kroy-basic-calculator.herokuapp.com/
+    // https://github.com/Kroy665/basic-calculator.git
     return(
         <Card style={{ width: '18rem', display: 'flex', justifyContent: 'center', marginTop: '2rem',backgroundColor: '#1f594f' }}>
             <Card.Img variant="top" src={props.CardImg} style={{padding: '5px', borderRadius: '15px'}}/>
@@ -9,7 +15,11 @@ function ProjectCard(props){
                 <Card.Text style={{fontSize: '15px', color: 'white', fontFamily: 'Helvetica'}}>
                     {props.CardText}
                 </Card.Text>
-                <Button variant="primary">See Project</Button>
+                <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                <Button onClick={() => openInNewTab(props.SiteUrl)} variant="success">See Project</Button>
+                <Button onClick={() => openInNewTab(props.GitUrl)} variant="info">Goto Github</Button>
+                </div>
+                
             </Card.Body>
         </Card>
     )
@@ -25,7 +35,9 @@ function Projects(props) {
                     <ProjectCard 
                         CardImg={`${process.env.PUBLIC_URL}/images/CardImg1.jpeg`}
                         CardTitle='Calculatar'
-                        CardText="This Is A CAlcularar" 
+                        CardText="This Is A CAlcularar"
+                        SiteUrl="https://kroy-basic-calculator.herokuapp.com"
+                        GitUrl="https://github.com/Kroy665/basic-calculator.git"
                     />
                 </Col>
                 
@@ -34,6 +46,8 @@ function Projects(props) {
                         CardImg={`${process.env.PUBLIC_URL}/images/CardImg1.jpeg`}
                         CardTitle='Calculatar'
                         CardText="This Is A CAlcularar" 
+                        SiteUrl="https://kroy-basic-calculator.herokuapp.com"
+                        GitUrl="https://github.com/Kroy665/basic-calculator.git"
                     />
                 </Col>
                 <Col style={{display: 'flex', justifyContent: 'center'}}>
@@ -41,6 +55,8 @@ function Projects(props) {
                         CardImg={`${process.env.PUBLIC_URL}/images/CardImg1.jpeg`}
                         CardTitle='Calculatar'
                         CardText="This Is A CAlcularar" 
+                        SiteUrl="https://kroy-basic-calculator.herokuapp.com"
+                        GitUrl="https://github.com/Kroy665/basic-calculator.git"
                     />
                 </Col>
             </Row>
